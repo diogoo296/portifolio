ad7cb1a4737f50b13fbb6f16c6a7b099aec3be36
Diogo Santana <diogo296@gmail.com>
2021-11-02 11:14:10 +0100

    Added AppBar (top navbar) component.

---
+++ src/components/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <v-tabs>
+      <v-tab>Home</v-tab>
+      <v-tab>About</v-tab>
+      <v-tab>Skills</v-tab>
+      <v-tab>Experience</v-tab>
+      <v-tab>Contact</v-tab>
+    </v-tabs>
+  </v-app-bar>
+</template>
